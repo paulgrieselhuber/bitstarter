8418f8e25471215e47ae9d81fe5e3ba184b8be02
Paul Grieselhuber <paul@loudnoises.us>
2013-07-28 19:56:33 +0000

    removed commented code

--- web.js
+++ web.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var htmlfile = "index.html";
+var robots = "robots.txt";
 
-var homePage = fs.readFileSync('index.html');
+app.use(express.static(__dirname));
 
 app.get('/', function(request, response) {
-  response.send(homePage.toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
